4a8c4ab077d76bbc3905dba8a63647f9c9eb1b8a
Adam Bechtold <adamcbechtold@gmail.com>
2024-02-20 13:13:29 -0500

    so we have import statements

--- algorithms/ReverseLinkedList.py
+++ algorithms/ReverseLinkedList.py
@@ -6,12 +6,14 @@ date: 2024.02.07
 whiteboard: https://link.excalidraw.com/l/ATbTqvQ19d9/LZoRQUB9Ke
 link: https://leetcode.com/problems/reverse-linked-list/
 """
+from typing import Optional, Tuple
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         return self.reverse_list_recursive(head)
